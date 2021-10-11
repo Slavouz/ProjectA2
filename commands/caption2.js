d184ed7e835ba365204bd6f3e03f5f87a993bce2
Slavouz Corp <48738722+Slavouz@users.noreply.github.com>
2021-10-11 07:34:24 +0000

    New Commands

---
+++ commands/caption2.js
@@ -0,0 +1,42 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const Canvas = require('canvas');
+const { registerFont } = require('canvas');
+const Discord = require('discord.js');
+registerFont('./ARIAL.TTF', { family: 'Arial' });
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('caption2')
+    .addStringOption(option => option.setName('img-url').setDescription('Masukkan url gambar').setRequired(true))
+    .addStringOption(option => option.setName('caption-text').setDescription('Masukkan tulisan').setRequired(true))
+  	.setDescription('Caption, tapi dibawah.'),
+	async execute(interaction, client) {
+    try{
+      const imgurl = interaction.options.getString('img-url'); 
+      const captext = interaction.options.getString('caption-text');
+
+      const background = await Canvas.loadImage(imgurl);
+      const canvas = Canvas.createCanvas(background.naturalWidth, background.naturalHeight + 50);
+      const context = canvas.getContext('2d');
+      context.fillStyle = "#FFFFFF";
+      context.fillRect(0, 0, canvas.width, canvas.height);
+      context.drawImage(background, 0, 0, background.naturalWidth, background.naturalHeight);
+      
+      context.fillStyle = "#000000";
+      let fontSize = 30;
+        do {
+          context.font = `${fontSize -= 2}px Arial`;
+        } while (context.measureText(captext).width > canvas.width);
+      //context.font = '30px Arial';
+      context.fillText(captext, 10, canvas.height - (canvas.height / 20));
+
+      const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'capt2.png');
+      //console.log(captext);
+      return interaction.reply({files: [attachment]});
+    }
+    catch(err){
+      return interaction.reply({content: `**Error: **${err}`, ephemeral: true});
+      console.log(err);
+    }
+	},
+};
